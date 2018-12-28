065ecf9d22cbf6e0182f22cc6d82ed30c792a397
Quoc Toan Minh Luong <quoctoan.luong@baesystems.com>
2018-12-27 16:03:44 -0800

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Mimics the cached mean of a vector code but excludes use of one line function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cached_inverse <- NULL
+  set <- function(y){
+    x <<- y
+    cached_inverse <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  set_inverse <- function(inversed){
+    cached_inverse <<- inversed
+  }
+  get_inverse <- function(){
+    cached_inverse
+  }
+  list( set = set, get = get,
+        set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Mimics the cached mean of a vector code to solve but error case first
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inversed <- x$get_inverse()
+        if(is.null(inversed)){
+          message("calculating new data")
+          data <- x$get()
+          inversed <- solve(data)
+          x$set_inverse(inversed)
+        }
+        else{
+          message("getting cached data")
+        }
+        inversed
 }
